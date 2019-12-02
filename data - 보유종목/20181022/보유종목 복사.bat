741a592fe25ecb9620bb52a17809c92d57f2f8ed
thdtjsdn <thdtjsdn@gmail.com>
2019-12-02 11:46:55 +0900

    N

---
+++ data - 보유종목/20181022/보유종목 복사.bat
@@ -0,0 +1,4 @@
+copy /y "E:\thdtjsdn\GitHub_Ttw\Ttw-Electron-Stock--NewsBoard\data - 보유종목\20181022\kosd.json" "E:\thdtjsdn\GitHub_Ttw\Ttw-Electron-Stock--NewsBoard\data\kosd.json"
+
+copy /y "E:\thdtjsdn\GitHub_Ttw\Ttw-Electron-Stock--NewsBoard\data - 보유종목\20181022\kosp.json" "E:\thdtjsdn\GitHub_Ttw\Ttw-Electron-Stock--NewsBoard\data\kosp.json"
+
---
+++ data - 보유종목/보유종목 복사.bat
@@ -0,0 +1,4 @@
+copy /y "E:\thdtjsdn\GitHub_Ttw\Ttw-Electron-Stock--NewsBoard\data - 보유종목\20190802\kosd.json" "E:\thdtjsdn\GitHub_Ttw\Ttw-Electron-Stock--NewsBoard\data\kosd.json"
+
+copy /y "E:\thdtjsdn\GitHub_Ttw\Ttw-Electron-Stock--NewsBoard\data - 보유종목\20190802\kosp.json" "E:\thdtjsdn\GitHub_Ttw\Ttw-Electron-Stock--NewsBoard\data\kosp.json"
+
